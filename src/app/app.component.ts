38d872f05b39b0f75e809587d1ed2cdc901f5ffe
jhons1206 <webdeveloper84@gmail.com>
2024-03-18 00:09:09 -0500

    obtencion de valores

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,33 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = '061 formIntro';
+  // title = '061 formIntro';
+  title = '062 formValores';
+
+  codigo: string = '';
+  nombre: string = '';
+  edad: number = 20;
+  opcion: string = 'Uno';
+  sexo: string = 'Hombre';
+  comentarios: string = '';
+  activar: boolean = true;
+
+  mostrarVar() {
+    console.log(
+      'Codigo (' + this.codigo + ')'
+      + 'Nombre (' + this.nombre + ')'
+      + 'Edad (' + this.edad + ')'
+      + 'Opcion (' + this.opcion + ')'
+      + 'Sexo (' + this.sexo + ')'
+      + 'Comentarios (' + this.comentarios + ')'
+      + 'Activar (' + this.activar + ')'
+    );
+  }
+
+  onSubmit() {
+    console.log('Submit form');
+  }
 }
